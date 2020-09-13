eb77a8270838b01eb14586b0fd3636081e6a98eb
Cale <caleshapera@gmail.com>
2020-09-12 18:45:07 -0700

    Updates

---
+++ precache-manifest.c1dcc37dab4d8b771f4ba101d49dc4ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "305ea68344b26fe52443b361c9a70077",
+    "url": "/fivestraight/index.html"
+  },
+  {
+    "revision": "910c37a2ceb99c7f9034",
+    "url": "/fivestraight/static/js/2.6d2b9492.chunk.js"
+  },
+  {
+    "revision": "f65b45f562155f0fa2653b346e3552a6",
+    "url": "/fivestraight/static/js/2.6d2b9492.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b1a3423b593f2c07bc4",
+    "url": "/fivestraight/static/js/main.b7eb6307.chunk.js"
+  },
+  {
+    "revision": "6a48d4fd638eab0adeb2",
+    "url": "/fivestraight/static/js/runtime-main.486f4968.js"
+  }
+]);
